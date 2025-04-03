3b6f784d470f766ca0d287b62e2dee2d6010a888
furlanjamily <furlanjamily@gmail.com>
2025-04-03 00:16:53 -0300

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
+import { useState } from 'react';
 import imageGithub from './assets/imageGithub.png';
+import Search from './assets/Search.png';
 
-function App() {
+
+export default function App() {
+  const [user, setUser] = useState("");
+
+  const handleSearch = () => {
+    console.log(`Procurando pelo usuário: ${user}`);
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen w-full">
-      <div className="text-center p-6 bg-gray-800 rounded-lg shadow-lg">
-        <img src={imageGithub} alt="Imagem GitHub"  className="mx-auto" />
-        <p className="text-white mt-4">Perfil</p>
+    <div className="flex w-full h-screen justify-center items-center bg-[#1F1F1F]">
+      <div className="flex flex-col w-full h-full max-w-[1156px] max-h-[537px] items-center bg-black">
+
+        {/* Cabeçalho */}
+        <div className="flex w-[384px] h-[82] items-center text-center pt-[39px]">
+          <div className='h-[58px] w-[58px] mr-[11px]'>
+            <img src={imageGithub} alt="Imagem GitHub" className="h-full w-full" />
+          </div>
+          <p className="flex text-white text-6xl font-semibold">
+            Perfil <span className='ml-[11px]'>GitHub</span>
+          </p>
+
+          
+        </div>
+
+
+        {/* Campo de pesquisa */}
+        <div className="mt-[27px]">
+          <input
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            placeholder="Digite um usuário do Github"
+            className="w-[503px] h-[62px] p-4 text-xl font-semibold bg-white"
+          />
+          <button onClick={handleSearch}>
+          <img src={Search} alt="Imagem Pesquisa" className="h-full w-full" />
+          </button>
+
+        </div>
+
+
       </div>
     </div>
   );
 }
 
-export default App;
